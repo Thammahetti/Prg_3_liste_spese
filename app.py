76ef5f1cd80efdfca1d731daea09cd0227bf801a
Thammahetti Thejan <thammahetti.m.t@iisgalvanimi.edu.it>
2024-11-28 12:43:05 +0000

    Finito Prog3

--- app.py
+++ app.py
@@ -1,26 +1,48 @@
 from flask import Flask, render_template,request,redirect,url_for
-lista_spese = []
+from flask_sqlalchemy import SQLAlchemy
+from models import db, ListaSpesa
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+lista_spese = []
+
 
 @app.route('/')
 def home():
-    return render_template('index.html' , lista_spese = lista_spese) 
+    lista_spesa = ListaSpesa.query.all()
+
+    return render_template('index.html' , lista_spese = lista_spesa) 
 
 @app.route('/add', methods=['POST'])
 def add():
-    product = request.form['product']
-    if product:
-        lista_spese.append(product)
+    elemento = request.form['elemento']
+    if elemento:
+        nuovo_elemento = ListaSpesa(elemento=elemento)
+        db.session.add(nuovo_elemento) 
+        db.session.commit() 
+       
     return redirect(url_for('home'))
+
 @app.route('/remove/<int:indice>', methods=['POST'])
 def remove(indice):
-    if 0 <= indice < len(lista_spese):
-        lista_spese.pop(indice)
+    elemento = ListaSpesa.query.get_or_404(indice)
+    db.session.delete(elemento)
+    db.session.commit() 
 
     return redirect(url_for('home'))
+
 @app.route('/delete', methods=['POST'])
 def delete():
-    lista_spese.clear()
+    ListaSpesa.query.delete() 
+    db.session.commit() 
 
     return redirect(url_for('home'))
 
